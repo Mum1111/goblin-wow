7e6b4baf5453f81eb14ba530dede564765a804d2
shihongjin <543528285@qq.com>
2022-05-23 15:34:20 +0800

    feat: 布局

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import * as React from "react";
 import {  useRoutes } from "react-router-dom";
 import routeConfig from "./config/routes";
+import Header from "./layout/Header";
 
 
 
@@ -10,7 +11,7 @@ function App() {
   const element = useRoutes(routeConfig)
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
+      <Header></Header>
       {element}
     </div>
   );
