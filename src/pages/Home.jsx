a7f169c1a6adb2a9ccc0749f08363c7ab2db616e
shihongjin <543528285@qq.com>
2022-05-23 14:49:33 +0800

    feat: 引入antd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import * as React from "react";
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Button } from "antd-mobile";
 
 function Home() {
-    return (
-      <>
-        <main>
-          <h2>Welcome to the homepage!</h2>
-          <p>You can do this, I believe in you.</p>
-        </main>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </>
-    );
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <Button color="primary" fill="solid">
+        Solid
+      </Button>
+    </>
+  );
 }
-  
+
 export default Home;
