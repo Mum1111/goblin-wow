daa6e8f0a6d734b9401683dab4a5f5029c2c339c
shihongjin <543528285@qq.com>
2022-05-23 17:09:06 +0800

    Revert "feat: 布局"
    This reverts commit 7e6b4baf5453f81eb14ba530dede564765a804d2.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import * as React from "react";
 import {  useRoutes } from "react-router-dom";
 import routeConfig from "./config/routes";
-import Header from "./layout/Header";
 
 
 
@@ -11,7 +10,7 @@ function App() {
   const element = useRoutes(routeConfig)
   return (
     <div className="App">
-      <Header></Header>
+      <h1>Welcome to React Router!</h1>
       {element}
     </div>
   );
