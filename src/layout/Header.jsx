99a348c2b56b87730afa4c19cf90c057418215ec
shihongjin <543528285@qq.com>
2022-05-23 17:33:44 +0800

    feat: 头部导航栏

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,96 @@
-import React from "react";
-import header from './header.module.scss'
 
-const Header = () => {
-    return (
-        <>
-            <div className={header.name}>hhhhh</div>
-        </>
-    )
-}
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./header.scss";
+// import "../../assest/font/iconfont.css";
+const Navbar = () => {
+  const [navIndex, setNavIndex] = useState("");
 
-export default Header
+  let history = useNavigate();
+  const saveIndex = (item) => {
+    setNavIndex(item);
+  };
+  return (
+    <>
+      <nav>
+        <div className="navigation-buttons">
+          <div
+            className="button-icon"
+            onClick={() => {
+              history(-1);
+              setTimeout(()=> {
+                setNavIndex(window.location.pathname)
+              },0)
+            }}
+          >
+            <div className="font-icon">
+              <i className="iconfont icon-arrowleft"></i>
+            </div>
+          </div>
+          <div
+            className="button-icon"
+            onClick={() => {
+              history(1);
+              setTimeout(()=> {
+                setNavIndex(window.location.pathname)
+              },0)
+            }}
+          >
+            <div className="font-icon">
+              <i className="iconfont icon-arrowright"></i>
+            </div>
+          </div>
+        </div>
+        <div className="navigation-links">
+          <div
+            className="link"
+            style={{
+              color: navIndex === "/" && window.location.pathname === '/' ? "#335eea" : "#000",
+            }}
+            onClick={() => {
+              saveIndex("/");
+            }}
+          >
+            <Link to="/">全部</Link>
+          </div>
+          <div
+            className="link"
+            style={{
+              color: navIndex === "/sale" && window.location.pathname === '/sale' ? "#335eea" : "#000",
+            }}
+            onClick={() => {
+              saveIndex("/sale");
+            }}
+          >
+            <Link to="/sale">折扣</Link>
+          </div>
+          <div
+            className="link"
+            style={{
+              color: navIndex === "/wish" && window.location.pathname === '/wish' ? "#335eea" : "#000",
+            }}
+            onClick={() => {
+              saveIndex("/wish");
+            }}
+          >
+            <Link to="/wish">心愿单</Link>
+          </div>
+        </div>
+        <div className="right-part">
+          <div className="search-box">
+            <div className="container">
+              <div className="input">
+                <div className="search-icon">
+                  <i className="iconfont icon-search"></i>
+                </div>
+                <input type="search" placeholder={"搜索"} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
